0a65bf80480cac16f59f1f2592686cafbc059607
Sabin Acharya <sabin@infodev.tech>
2019-07-22 17:56:29 +0545

    activation_map

--- layer_output.py
+++ layer_output.py
@@ -27,8 +27,24 @@ def plot_feature_maps(feature_maps):
     #     plt.imshow(feature_maps[:,:,i])
     #     plt.title('feature map {}'.format(i+1))
     # plt.show()
-    plt.imshow(feature_maps[:, :, 20])
-    plt.show()
+    """ Specific feature
+    """
+    # plt.imshow(feature_maps[:, :, 20])
+    # print(feature_maps[:,:,20].shape)
+    # plt.show()
+
+    """ Combined """
+    map_h,map_w = feature_maps[:, :, 20].shape
+    map = np.zeros((map_h,map_w),np.float32)
+    height, width, depth = feature_maps.shape
+    for i in range(depth):
+        map+=feature_maps[:,:,i]
+    # plt.imshow(map)
+    # plt.show()
+    return map
+
+
+
 
 
 
@@ -36,11 +52,19 @@ def plot_feature_maps(feature_maps):
 model = load_model('model.h5')
 model_layers = [ layer.name for layer in model.layers]
 print(model_layers)
-features_extractor = Model(inputs=model.input, outputs=model.get_layer(model_layers[0]).output)
+features_extractor = Model(inputs=model.input, outputs=model.get_layer(model_layers[4]).output)
 
-orig = cv2.imread('test/17.jpg')
+orig = cv2.imread('test/3.jpg')
+h,w,_ = orig.shape
 image = cv2.resize(orig, (124, 124))
 image = image.astype("float") / 255.0
 image = np.expand_dims(image, axis=0)
 feature_maps = features_extractor.predict(image)[0]
-plot_feature_maps(feature_maps)
+activation_map = plot_feature_maps(feature_maps)
+# print(activation_map)
+plt.subplot(1, 2,1)
+plt.imshow(orig)
+plt.subplot(1, 2,2)
+plt.imshow(cv2.resize(activation_map,(w,h)))
+plt.savefig('activation/data.png')
+plt.show()
